4b095e2e76e05e4cfc671bc82c38f089c319a47a
sergii-bondariev <SergeyBondarev@gmail.com>
2015-06-16 08:38:34 -0700

    a

---
+++ data_wrangling/simple.py
@@ -0,0 +1,51 @@
+# Your task is to read the input DATAFILE line by line, and for the first 10 lines (not including the header)
+# split each line on "," and then for each line, create a dictionary
+# where the key is the header title of the field, and the value is the value of that field in the row.
+# The function parse_file should return a list of dictionaries,
+# each data line in the file being a single list entry.
+# Field names and values should not contain extra whitespace, like spaces or newline characters.
+# You can use the Python string method strip() to remove the extra whitespace.
+# You have to parse only the first 10 data lines in this exercise,
+# so the returned list should have 10 entries!
+import os
+
+DATADIR = ""
+DATAFILE = "beatles-diskography.csv"
+
+
+def parse_file(datafile):
+    data = []
+    with open(datafile, "r") as f:
+        #get rid of extra space character including newline
+        titles = f.readline().strip()
+        titles = titles.split(',')
+        i = 1
+        for line in f:
+            if (i == 11):
+                break 
+            i += 1
+            line = line.replace('\xe2\x80\x94', '-')
+            line = line.strip().split(',')
+            d = {}
+
+            #for j in range(0, len(titles)):
+            for j, value in enumerate(line):
+                d[titles[j].strip()] = value.strip()
+
+            data.append(d)
+
+    return data
+
+
+def test():
+    # a simple test of your implemetation
+    datafile = os.path.join(DATADIR, DATAFILE)
+    d = parse_file(datafile)
+    firstline = {'Title': 'Please Please Me', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '22 March 1963', 'US Chart Position': '-', 'RIAA Certification': 'Platinum', 'BPI Certification': 'Gold'}
+    tenthline = {'Title': '', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '10 July 1964', 'US Chart Position': '-', 'RIAA Certification': '', 'BPI Certification': 'Gold'}
+
+    assert d[0] == firstline
+    assert d[9] == tenthline
+
+    
+test()
