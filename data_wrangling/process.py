73ba97ad7d72184b9e6f994a0d6231773b00202f
sergii-bondariev <SergeyBondarev@gmail.com>
2015-06-23 08:46:26 -0700

    a

---
+++ data_wrangling/process.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Let's assume that you combined the code from the previous 2 exercises
+# with code from the lesson on how to build requests, and downloaded all the data locally.
+# The files are in a directory "data", named after the carrier and airport:
+# "{}-{}.html".format(carrier, airport), for example "FL-ATL.html".
+# The table with flight info has a table class="dataTDRight".
+# There are couple of helper functions to deal with the data files.
+# Please do not change them for grading purposes.
+# All your changes should be in the 'process_file' function
+# This is example of the datastructure you should return
+# Each item in the list should be a dictionary containing all the relevant data
+# Note - year, month, and the flight data should be integers
+# You should skip the rows that contain the TOTAL data for a year
+# data = [{"courier": "FL",
+#         "airport": "ATL",
+#         "year": 2012,
+#         "month": 12,
+#         "flights": {"domestic": 100,
+#                     "international": 100}
+#         },
+#         {"courier": "..."}
+# ]
+from bs4 import BeautifulSoup
+from zipfile import ZipFile
+import os
+
+datadir = "data"
+
+
+def open_zip(datadir):
+    with ZipFile('{0}.zip'.format(datadir), 'r') as myzip:
+        myzip.extractall()
+
+
+def process_all(datadir):
+    files = os.listdir(datadir)
+    return files
+
+
+def process_file(f):
+    """This is example of the data structure you should return.
+    Each item in the list should be a dictionary containing all the relevant data
+    from each row in each file. Note - year, month, and the flight data should be 
+    integers. You should skip the rows that contain the TOTAL data for a year
+    data = [{"courier": "FL",
+            "airport": "ATL",
+            "year": 2012,
+            "month": 12,
+            "flights": {"domestic": 100,
+                        "international": 100}
+            },
+            {"courier": "..."}
+    ]
+    """
+    data = []
+    info = {}
+    info["courier"], info["airport"] = f[:6].split("-")
+    
+
+    with open("{}/{}".format(datadir, f), "r") as html:
+
+        soup = BeautifulSoup(html)
+        for row in soup.find_all('tr', class_ = "dataTDRight"):
+            item = info.copy()
+            cols = []
+
+            for col in row.find_all('td'):
+                cols.append(col)
+
+            if cols[1].text != "TOTAL":
+                item["year"] = int(cols[0].text)
+                item["month"] = int(cols[1].text)
+                fl = {}
+                fl["domestic"] = int(cols[2].text.replace(',', ""))
+                fl["international"] = int(cols[3].text.replace(',', ""))
+                item["flights"] = fl
+
+                data.append(item)
+
+    return data
+
+
+def test():
+    print "Running a simple test..."
+    #open_zip(datadir)
+    files = process_all(datadir)
+    data = []
+    for f in files:
+        data += process_file(f)
+        
+    assert len(data) == 399  # Total number of rows
+    for entry in data[:3]:
+        assert type(entry["year"]) == int
+        assert type(entry["month"]) == int
+        assert type(entry["flights"]["domestic"]) == int
+        assert len(entry["airport"]) == 3
+        assert len(entry["courier"]) == 2
+    assert data[-1]["airport"] == "ATL"
+    assert data[-1]["flights"] == {'international': 108289, 'domestic': 701425}
+    
+    print "... success!"
+
+if __name__ == "__main__":
+    test()
